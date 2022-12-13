6152881b6d9e55de5e1485cfb916e4acd6675b74
Ibrahimova11 <ca.r215.14@ca.local>
2022-12-13 01:46:56 -0800

    Added project

---
+++ WeaponProject/Models/Weapon.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponProject.Models
+{
+    internal class Weapon
+    {
+        int _bulletCapacity;
+        int _remainingBulletCount;
+        int _bulletEmptyTime;
+        string _fireMode;
+        public int BulletCapacity
+        {
+            get { return _bulletCapacity; }
+            set
+            {
+                _bulletCapacity = SetCapacity(value);
+            }
+        }
+        public int RemainingBulletCount
+        {
+            get { return _remainingBulletCount; }
+            set { _remainingBulletCount = SetRemain(value); }
+        }
+        public int BulletEmptyTime
+        {
+            get { return _bulletEmptyTime; }
+            set { _bulletEmptyTime = SetEmptyTime(value); }
+        }
+        public string FireMode
+        {
+            get { return _fireMode; }
+            set { _fireMode =SetFireMode(value); }
+        }
+
+        public void Shoot()
+        {
+            if (RemainingBulletCount > 0)
+            {
+                RemainingBulletCount -= 1;
+                Console.WriteLine("Shot one bullet.");
+            }
+            else
+            {
+                Console.WriteLine("Comb is empty.");
+                return;
+            }
+        }
+        public void Fire()
+        {
+            if (RemainingBulletCount > 0)
+            {
+                if (FireMode == "auto")
+                {
+                    int time = RemainingBulletCount * BulletEmptyTime / BulletCapacity;
+                    RemainingBulletCount = 0;
+                    Console.WriteLine($"Comb fully emptied in {time} seconds.");
+                }
+                else
+                {
+                    RemainingBulletCount -= 1;
+                    Console.WriteLine("Fire mode is single,shot one bullet.");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Comb is empty.");
+                return;
+            }
+
+        }
+        public int GetRemainBulletCount()
+        {
+            return BulletCapacity - RemainingBulletCount;
+        }
+        public void Reload()
+        {
+            _remainingBulletCount = _bulletCapacity;
+            Console.WriteLine("Comb reloaded.");
+            return;
+
+        }
+        public void ChangeFireMode()
+        {
+            if (FireMode == "auto")
+            {
+                Console.WriteLine("Fire mode was auto,changed to single.");
+                FireMode = "single";
+            }
+            else
+            {
+                Console.WriteLine("Fire mode was single, changed to automatic.");
+                FireMode = "auto";
+            }
+            return;
+
+        }
+        public Weapon(int bulletCapacity,int remainingBulletCount, int bulletEmptyTime,string fireMode)
+        {
+            BulletCapacity = bulletCapacity;
+            RemainingBulletCount = remainingBulletCount;
+            BulletEmptyTime = bulletEmptyTime;
+            FireMode = fireMode;
+        }
+        public  int SetCapacity(int value)
+        {
+            Console.Write("Enter comb capacity ");
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (!String.IsNullOrEmpty(str))
+                {
+                    int.TryParse(str, out value);
+                    if (value >= 0 && value <= 1000)
+                    {
+                        return value;
+                    }
+                    Console.WriteLine("Wrong input,enter again");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input,enter again");
+                }
+            }
+        }
+        public int SetRemain(int value)
+        {
+            Console.Write("Enter remaining bullets ");
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (!String.IsNullOrEmpty(str))
+                {
+                    int.TryParse(str, out value);
+                    if (value >= 0  && value <= BulletCapacity)
+                    {
+                        return value;
+                    }
+                    Console.WriteLine("Wrong input,enter again");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input,enter again");
+                }
+            }
+        }
+        public int SetEmptyTime(int value )
+        {
+            Console.Write("Enter empty time ");
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (!String.IsNullOrEmpty(str))
+                {
+                    int.TryParse(str, out value);
+                    if (value > 0)
+                    {
+                        return value;
+                    }
+                    Console.WriteLine("Wrong input,enter again");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input,enter again");
+                }
+            }
+        }
+        public string SetFireMode(string value)
+        {
+            Console.Write("Enter fire mode auto/single ");
+            string str;
+            while (true)
+            {
+                str = Console.ReadLine();
+                str = str.Trim().ToLower();
+                if (!String.IsNullOrEmpty(str))
+                {
+                    if (str == "auto" || str == "single")
+                    {
+                        return str;
+                    }
+                    Console.WriteLine("Wrong input,enter again");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input,enter again");
+                }
+            }
+        }
+    }
+}
---
+++ WeaponProject/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.Design;
+using WeaponProject.Models;
+
+namespace WeaponProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = 0;
+            int remain = 0;
+            int emptyTime = 0 ;
+            string mode = "";
+            int? option;
+            Weapon weapon = new Weapon(capacity, remain, emptyTime, mode);
+            Menu:
+            Console.WriteLine("0 - Menu\n1- Shoot\n2-Fire\n3-Get remaining bullets\n4-Reload\n5- Change fire mode\n6-quit");
+            while (true)
+            {
+                option = Convert.ToInt32(Console.ReadLine());
+                if (option != null && option >=0 && option <=6)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input");
+                }
+            }
+            switch (option)
+            {
+                case 0:
+                    goto Menu;
+                case 1:
+                    weapon.Shoot();
+                    break;
+                case 2:
+                    weapon.Fire();
+                    break;
+                case 3:
+                    weapon.GetRemainBulletCount();
+                    break;
+                case 4:
+                    weapon.Reload();
+                    break;
+                case 5:
+                    weapon.ChangeFireMode();
+                    break;
+                case 6:
+                    Console.WriteLine("Program ended.");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
